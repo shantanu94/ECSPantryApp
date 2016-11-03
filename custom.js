8e9c5ab865164a480994d57260a16e34a30500e3
shantanu94 <bhardwaj.35@osu.edu>
2016-11-03 13:06:31 -0400

    added css and js

---
+++ custom.js
@@ -0,0 +1,12 @@
+function doneClicked() {
+	var zeroToOne = document.getElementById("0to1").value;
+	var twoToThree = document.getElementById("2to3").value;
+	var fourToSix = document.getElementById("4to6").value;
+	var sevenToNine = document.getElementById("7to9").value;
+	var TenToThirteen = document.getElementById("10to13").value;
+	var FourteenToSeventeen = document.getElementById("14to17").value;
+	var eighteeToSixty = document.getElementById("18to60").value;
+	var sixtyOnePlus = document.getElementById("61Plus").value;
+
+	document.getElementById("BeanValue").value = zeroToOne;
+}
